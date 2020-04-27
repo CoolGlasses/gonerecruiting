ccd783f63baf153918ca8d25fa3749b999ed9637
CoolGlasses <robinandrea_2@yahoo.com>
2020-04-27 11:17:05 -0700

    fixed syntax errors with the players.js file -- this file is now usable

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -1,11 +1,10 @@
-<script>
  function searchFunction() {
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; {
+   for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[0];
     if (td) {
         txtValue = td.textContent || td.innerText;
@@ -16,5 +15,4 @@
       }
     }
   }
-}
-</script>
+ }
