8b1655843caf1dd203d12eead1f7d00f9f91ff2a
Robin Andrea <robinandrea_2@yahoo.com>
2020-05-14 17:35:38 -0700

    added filter methods for filtering by 1 column only, and added necessary curate_height helper method

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -54,84 +54,6 @@ class PlayersController < ApplicationController
     def destroy
     end
 
-    # def curate_height(players)
-    #     players.each do |player|
-    #         height = player.height
-    #         case height
-    #         when ""
-    #             height = 0
-    #         when "4-5","4-05"
-    #             height = 53
-    #         when "4-6","4-06"
-    #             height = 54
-    #         when "4-7","4-07"
-    #             height = 55
-    #         when "4-8","4-08"
-    #             height = 56
-    #         when "4-9","4-09"
-    #             height = 57
-    #         when "4-10"
-    #             height = 58
-    #         when "4-11"
-    #             height = 59
-    #         when "5-0","5-00"
-    #             height = 60
-    #         when "5-1","5-01"
-    #             height = 61
-    #         when "5-2","5-02"
-    #             height = 62
-    #         when "5-3","5-03"
-    #             height = 63
-    #         when "5-4","5-04"
-    #             height = 64
-    #         when "5-5","5-05"
-    #             height = 65
-    #         when "5-6","5-06"
-    #             height = 66
-    #         when "5-7","5-07"
-    #             height = 67
-    #         when "5-8","5-08"
-    #             height = 68
-    #         when "5-9","5-09"
-    #             height = 69
-    #         when "5-10"
-    #             height = 70
-    #         when "5-11"
-    #             height = 71
-    #         when "6-0","6-00"
-    #             height = 72
-    #         when "6-1","6-01"
-    #             height = 73
-    #         when "6-2","6-02"
-    #             height = 74
-    #         when "6-3","6-03"
-    #             height = 75
-    #         when "6-4","6-04"
-    #             height = 76
-    #         when "6-5","6-05"
-    #             height = 77
-    #         when "6-6","6-06"
-    #             height = 78
-    #         when "6-7","6-07"
-    #             height = 79
-    #         when "6-8","6-08"
-    #             height = 80
-    #         when "6-9","6-09"
-    #             height = 81
-    #         when "6-10"
-    #             height = 82
-    #         when "6-11"
-    #             height = 83
-    #         when "7-0","7-00"
-    #             height = 84
-    #         else
-    #             height = 0
-    #         end
-    #         player.height = height
-    #     end
-    #     return players
-    # end
-
     def grade_filter(grade)
         players = ActiveRecord::Base.connection.execute(<<-SQL)
         SELECT 
@@ -174,5 +96,89 @@ class PlayersController < ApplicationController
         return players
     end
 
+    def name_filter(name)
+        #capitalization matters!  Need to account for that with an OR in the WHERE clause
+        players = ActiveRecord::Base.connection.execute(<<-SQL)
+        SELECT 
+            players.name, players.position, players.height, players.grade, schools.name AS School
+        FROM
+            players
+        JOIN
+            teams
+        ON
+            players.team_id = teams.osaa_team_id
+        JOIN
+            schools
+        ON
+            teams.osaa_school_id = schools.osaa_school_id
+        WHERE
+            players.name LIKE '%#{name}%';
+        SQL
 
+        return players
+    end
+
+    def school_filter(school)
+        #capitalization matters!  Need to account for that with an OR in the WHERE clause
+        players = ActiveRecord::Base.connection.execute(<<-SQL)
+        SELECT 
+            players.name, players.position, players.height, players.grade, schools.name AS School
+        FROM
+            players
+        JOIN
+            teams
+        ON
+            players.team_id = teams.osaa_team_id
+        JOIN
+            schools
+        ON
+            teams.osaa_school_id = schools.osaa_school_id
+        WHERE
+            players.name LIKE '%#{school}%';
+        SQL
+
+        return players
+    end
+
+    def height_filter(height)
+        clean_height = curate_height(height)
+
+        if clean_height.length == 2
+            clean_height_1 = clean_height[0]
+            clean_height_2 = clean_height[1]
+        else
+            clean_height_1 = clean_height
+            clean_height_2 = clean_height
+        end
+
+        players = ActiveRecord::Base.connection.execute(<<-SQL)
+        SELECT 
+            players.name, players.position, players.height, players.grade, schools.name AS School
+        FROM
+            players
+        JOIN
+            teams
+        ON
+            players.team_id = teams.osaa_team_id
+        JOIN
+            schools
+        ON
+            teams.osaa_school_id = schools.osaa_school_id
+        WHERE
+            players.height='#{clean_height_1}' 
+        OR
+            players.height='#{clean_height_2}';
+        SQL
+
+        return players
+    end
+
+    def curate_height(height)
+        if height.empty?
+            return height
+        elsif height.length == 3
+            curated_height = height.gsub("-", "-0")
+            return [height, curated_height]
+        end
+    end
 end
