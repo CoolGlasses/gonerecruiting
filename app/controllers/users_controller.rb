3b11c0677005b83689105de8eca33f8d5c38e70c
Robin Andrea <robinandrea_2@yahoo.com>
2020-05-17 14:27:21 -0700

    completed initial user auth pattern from app academy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,25 @@ class UsersController < ApplicationController
     def index
         render :new
     end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors.full_messages
+        end
+    end
+
+    def new
+        @user = User.new
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+    attr_reader :password
+
+    validates :username, presence: true
+    validates :password_digest, presence: { message: 'Password can\'t be blank' }
+    validates :password, length: { minimum: 6, allow_nil: true }
+    
     def password=(password)
         self.password_digest = BCrypt::Password.create(password)
     end
