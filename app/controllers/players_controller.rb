23408f1d7aaeef3e1ef341c7266a1c326d835774
Robin Andrea <robinandrea_2@yahoo.com>
2020-05-25 09:16:58 -0700

    added LOWER logic to name and school filter methods to allow for searching of variations of caps

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -25,13 +25,13 @@ class PlayersController < ApplicationController
     end
     
     def filter
-        name = params["filter"]["name"]
+        player_name = params["filter"]["name"]
         height = params["filter"]["height"]
         position = params["filter"]["position"]
         school = params["filter"]["school"]
         grade = params["filter"]["grade"]
 
-        filter_array = [height, grade, position, school, name]
+        filter_array = [height, grade, position, school, player_name]
         remaining_filters = []
 
         filter_array.each do |param|
@@ -42,19 +42,14 @@ class PlayersController < ApplicationController
 
         if !height.nil?
             @players = height_filter(height)
-            remaining_filters.delete(height)
-        elsif grade != ""
+        elsif grade != "" && !grade.nil?
             @players = grade_filter(grade)
-            remaining_filters.delete(grade)
         elsif !position.nil?
             @players = position_filter(position)
-            remaining_filters.delete(position)
         elsif school != ""
             @players = school_filter(school)
-            remaining_filters.delete(school)
-        elsif name != ""
-            @players = name_filter(name)
-            remaining_filters.delete(name)
+        elsif player_name != "" && !player_name.nil?
+            @players = name_filter(player_name)
         end
 
         session[:remaining_filters] = filter_array
@@ -108,10 +103,7 @@ class PlayersController < ApplicationController
         return players
     end
 
-    def name_filter(name)
-        lower_case_name = name.downcase
-        capitalized = lower_case_name[0].upcase + lower_case_name[1..-1]
-        
+    def name_filter(name)       
         players = ActiveRecord::Base.connection.execute(<<-SQL)
         SELECT 
             players.name, players.position, players.height, players.grade, schools.name AS School
@@ -126,18 +118,13 @@ class PlayersController < ApplicationController
         ON
             teams.osaa_school_id = schools.osaa_school_id
         WHERE
-            players.name LIKE '%#{lower_case_name}%'
-        OR
-            players.name LIKE '%#{capitalized}%';
+            LOWER(players.name) LIKE LOWER('%#{name}%')
         SQL
 
         return players
     end
 
-    def school_filter(school)
-        first_school = school.downcase
-        second_school = first_school[0].upcase + first_school[1..-1]
-        
+    def school_filter(school)        
         players = ActiveRecord::Base.connection.execute(<<-SQL)
         SELECT 
             players.name, players.position, players.height, players.grade, schools.name AS School
@@ -152,9 +139,7 @@ class PlayersController < ApplicationController
         ON
             teams.osaa_school_id = schools.osaa_school_id
         WHERE
-            schools.name LIKE '%#{first_school}%'
-        OR
-            schools.name LIKE '%#{second_school}%';
+            LOWER(schools.name) LIKE LOWER('%#{school}%')
         SQL
 
         return players
