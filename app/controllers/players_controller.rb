c073a5d0517c3d886efb154e8b188f8de5c40815
CoolGlasses <robinandrea_2@yahoo.com>
2020-06-02 15:31:11 -0700

    utilized activerecord object and chaining query logic into filter method to establish functional logic to allow user to query the database with a dynamic where clause

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,4 @@
-require '/home/robin/gone_recruiting/db/convert_height.rb'
+require '/home/robin/gonerecruiting/db/convert_height.rb'
 
 class PlayersController < ApplicationController
 
@@ -10,174 +10,64 @@ class PlayersController < ApplicationController
             "<5ft 3in", "5ft 3in", "5ft 4in", "5ft 5in", "5ft 5in",
             "5ft 6in", "5ft 7in", "5ft 8in", "5ft 9in", "5ft 10in", "5ft 11in",
             "6ft 0in", "6ft 1in", "6ft 2in", "6ft 3in", "6ft 4in", ">6ft 4in"]
+        @positionsArray = ["G", "W", "F", "P", "C"]
+        @gradeArray = ["12", "11", "10", "9"]
     end
     
     def filter
-        player_name = params["filter"]["name"]
+        player_name = params["filter"]["name"].downcase
         height = params["filter"]["height"]
         position = params["filter"]["position"]
-        school = params["filter"]["school"]
+        school = params["filter"]["school"].downcase
         grade = params["filter"]["grade"]
 
-        filter_hash = {:height => height, :grade => grade, :position => position, 
-                        :school => school, :player_name => player_name}
-
-        filter_hash.each do |key, value|
-            if value.nil? || value == ""
-                filter_hash.except!(key)
+        if !height.nil?
+            converted_height = convert_height(height)
+            if @players.nil?
+                @players = Player.where("height_inches >= #{converted_height}")
+                
+            else
+                @players = @players.where("height_inches >= #{converted_height}")
             end
         end
 
-        if !height.nil?
-            @players = height_filter(height)
-            filter_hash.except!(:height)
-        elsif grade != "" && !grade.nil?
-            @players = grade_filter(grade)
-            filter_hash.except!(:grade)
-        elsif !position.nil?
-            @players = position_filter(position)
-            filter_hash.except!(:position)
-        elsif school != ""
-            @players = school_filter(school)
-            filter_hash.except!(:school)
-        elsif player_name != "" && !player_name.nil?
-            @players = name_filter(player_name)
-            filter_hash.except!(:player_name)
+        if grade != "" && !grade.nil?
+            if @players.nil?
+                @players = Player.where("grade = ?", grade)
+            else
+                @players = @players.where("grade = ?", grade)
+            end
         end
 
-        gon.watch.filtered = true
-        gon.watch.remaining_filters = filter_hash
+        if !position.nil?
+            if @players.nil?
+                @players = Player.where("position = ?", position)
+            else
+                @players = @players.where("position = ?", position)
+            end
+        end
+
+        if school != ""
+            if @players.nil?
+                @players = Player.where("LOWER(school_name) LIKE ?", "%#{school}%")
+            else
+                @players = @players.where("LOWER(school_name) LIKE ?", "%#{school}%")
+            end
+        end
+
+        if player_name != "" && !player_name.nil?
+            if @players.nil?
+                @players = Player.where("LOWER(name) LIKE ?", "%#{player_name}%")
+            else
+                @players = @players.where("LOWER(name) LIKE ?", "%#{player_name}%")
+            end
+        end
+
         @players.to_a
         render :index
     end
     
     def show
         render json: Player.find(params[:id])
-    end
-
-    def grade_filter(grade)
-        players = ActiveRecord::Base.connection.execute(<<-SQL)
-        SELECT 
-            players.name, players.position, players.height, players.grade, schools.name AS School
-        FROM
-            players
-        JOIN
-            teams
-        ON
-            players.team_id = teams.osaa_team_id
-        JOIN
-            schools
-        ON
-            teams.osaa_school_id = schools.osaa_school_id
-        WHERE
-            players.grade='#{grade}';
-        SQL
-
-        return players
-    end
-
-    def position_filter(position)
-        players = ActiveRecord::Base.connection.execute(<<-SQL)
-        SELECT 
-            players.name, players.position, players.height, players.grade, schools.name AS School
-        FROM
-            players
-        JOIN
-            teams
-        ON
-            players.team_id = teams.osaa_team_id
-        JOIN
-            schools
-        ON
-            teams.osaa_school_id = schools.osaa_school_id
-        WHERE
-            players.position='#{position}';
-        SQL
-
-        return players
-    end
-
-    def name_filter(name)       
-        players = ActiveRecord::Base.connection.execute(<<-SQL)
-        SELECT 
-            players.name, players.position, players.height, players.grade, schools.name AS School
-        FROM
-            players
-        JOIN
-            teams
-        ON
-            players.team_id = teams.osaa_team_id
-        JOIN
-            schools
-        ON
-            teams.osaa_school_id = schools.osaa_school_id
-        WHERE
-            LOWER(players.name) LIKE LOWER('%#{name}%')
-        SQL
-
-        return players
-    end
-
-    def school_filter(school)        
-        players = ActiveRecord::Base.connection.execute(<<-SQL)
-        SELECT 
-            players.name, players.position, players.height, players.grade, schools.name AS School
-        FROM
-            players
-        JOIN
-            teams
-        ON
-            players.team_id = teams.osaa_team_id
-        JOIN
-            schools
-        ON
-            teams.osaa_school_id = schools.osaa_school_id
-        WHERE
-            LOWER(schools.name) LIKE LOWER('%#{school}%')
-        SQL
-
-        return players
-    end
-
-    def height_filter(height)
-        converted_height = convert_height(height)
-
-        if converted_height != 62
-            players = ActiveRecord::Base.connection.execute(<<-SQL)
-            SELECT 
-                players.name, players.position, players.height, players.grade, schools.name AS School
-            FROM
-                players
-            JOIN
-                teams
-            ON
-                players.team_id = teams.osaa_team_id
-            JOIN
-                schools
-            ON
-                teams.osaa_school_id = schools.osaa_school_id
-            WHERE
-                players.height_inches >='#{converted_height}';
-            SQL
-            return players
-        else
-            players = ActiveRecord::Base.connection.execute(<<-SQL)
-            SELECT 
-                players.name, players.position, players.height, players.grade, schools.name AS School
-            FROM
-                players
-            JOIN
-                teams
-            ON
-                players.team_id = teams.osaa_team_id
-            JOIN
-                schools
-            ON
-                teams.osaa_school_id = schools.osaa_school_id
-            WHERE
-                players.height_inches <='#{converted_height}';
-            SQL
-            return players
-        end
     end
 end
