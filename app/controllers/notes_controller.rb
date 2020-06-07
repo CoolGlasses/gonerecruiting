32b000f64c54c92e024cc0f484c76d8be4e52ac1
Robin Andrea <robinandrea_2@yahoo.com>
2020-06-07 10:18:17 -0700

    added NotesController

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,12 @@
 class NotesController < ApplicationCcontroller
+    def create
+        @note = Note.new(note_params)
+
+        @note.save!
+    end
+
+    protected
+    def note_params
+        self.params.require(:note).permit(:user_id, :player_id, :note)
+    end
 end
