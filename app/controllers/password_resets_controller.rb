d466b1cbe102a2db91db565ef27382046c6cc23b
Robin Andrea <robinandrea_2@yahoo.com>
2020-06-26 18:04:20 -0700

    updated passord_resets_controller with giude from railstutorial.org on an appropriate pattern

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,35 +1,65 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,         only: [:edit, :update]
+  before_action :valid_user,       only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    user.send_password_reset if user
-    flash[:notice] = 'E-mail sent with password reset instructions.'
-    redirect_to new_session_path
-  end
-
-  def edit
-    @user = User.find_by_password_reset_token!(params[:id])
-  end
-
-  def update
-    @user = User.find_by_password_reset_token!(params[:id])
-    if @user.password_reset_sent_at < 2.hour.ago
-      flash[:notice] = 'Password reset has expired'
-      redirect_to new_password_reset_path
-    elsif @user.update(user_params)
-      flash[:notice] = 'Password has been reset!'
-      redirect_to new_session_path
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
     else
-      render :edit
+      flash.now[:danger] = "Email address not found"
+      render 'new'
     end
   end
 
-private
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def user_params
-    params.require(:user).permit(:password)
+  def edit
   end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    # Before filters
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Confirms a valid user.
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+
+    # Checks expiration of reset token.
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
 end
