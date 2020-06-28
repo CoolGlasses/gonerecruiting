4c4d834db5fe8e59d52a0ce885cd842c86a4481a
Robin Andrea <robinandrea_2@yahoo.com>
2020-06-28 10:32:29 -0700

    updated to ruby 2.7.1 and rails 6.0.3.2

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_030102) do
+ActiveRecord::Schema.define(version: 2020_06_28_172352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -166,6 +166,9 @@ ActiveRecord::Schema.define(version: 2020_06_15_030102) do
     t.datetime "updated_at", null: false
     t.string "session_token", null: false
     t.citext "email"
+    t.string "password_reset_token"
+    t.datetime "password_reset_sent_at"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
