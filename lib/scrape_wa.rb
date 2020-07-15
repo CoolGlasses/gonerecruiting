abeb552022f3e21ebb8bc139c614d1b84e881a5c
Robin Andrea <robinandrea_2@yahoo.com>
2020-07-14 20:59:29 -0700

    added initial logic for scrapping washington

--- lib/scrape_wa.rb
+++ lib/scrape_wa.rb
@@ -2,5 +2,52 @@ require "nokogiri"
 require "open-uri"
 require "pry"
 
-doc = Nokogiri::HTML(open('http://www.2bcentral.com/index.php?pid=0.31.245.12.320'))
+browser = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1'
+
+#go to starting point
+doc = Nokogiri::HTML(open('http://www.2bcentral.com/index.php?pid=0.31.245.12.320',
+                        'User-Agent' => '#{browser}'))
+
+#create an array of all possible sites
+@final_list_of_schools = []
+@list_of_schools = doc.css('div.wpa_navbarx').css('table').css('td').css('option')
+
+@list_of_schools.each do |school|
+    @final_list_of_schools << school["value"]
+end
+
+
+#go to every school
+@final_list_of_schools.each do |url|
+    @school = Nokogiri::HTML(open('#{url}', 'User-Agent' => '#{browser}'))
+#identify url of specifically the girls basketball sub-page..have to iterate throught the girls sports element in case a season (fall/winter/spring) is not present
+
+    @girls_url_nodes = @school.xpath('//*[@id="school_menu_container"]/table/tr/td[2]/div/div/table/tr[3]/td/a')
+    @girls_url_finally = ""
+    @girls_url_nodes.each do |node|
+        if node.text.to_s == "Basketball"
+            @girls_url_finally = node["href"]
+        end
+    end
+
+
+#go to the sub-page
+
+@roster_page = Nokogiri::HTML(open('#{@girls_url_finally}', 'User-Agent' => '#{browser}'))
+
+#add the roster to an array
+
+@roster_array = []
+
+@roster_html = @roster_page.css('#tbl_roster td')
+@roster_html.each do |data|
+    @roster_array << data.text
+end
+
+#iterate and create objects in the database associated
+
+#Home Away Height Position Year Games Pts Avg
+
+
+
 binding.pry
