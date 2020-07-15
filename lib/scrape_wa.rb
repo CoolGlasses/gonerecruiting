cfc1cd8b0e507dd63f1c3efefe4270f7bd8a26db
CoolGlasses <robinandrea_2@yahoo.com>
2020-07-15 15:59:10 -0700

    updated notes to scrape file

--- lib/scrape_wa.rb
+++ lib/scrape_wa.rb
@@ -18,36 +18,56 @@ end
 
 
 #go to every school
-@final_list_of_schools.each do |url|
-    @school = Nokogiri::HTML(open('#{url}', 'User-Agent' => '#{browser}'))
+# @final_list_of_schools.each do |url|
+#     @school = Nokogiri::HTML(open('#{url}', 'User-Agent' => '#{browser}'))
 #identify url of specifically the girls basketball sub-page..have to iterate throught the girls sports element in case a season (fall/winter/spring) is not present
 
-    @girls_url_nodes = @school.xpath('//*[@id="school_menu_container"]/table/tr/td[2]/div/div/table/tr[3]/td/a')
-    @girls_url_finally = ""
-    @girls_url_nodes.each do |node|
-        if node.text.to_s == "Basketball"
-            @girls_url_finally = node["href"]
-        end
-    end
+    # @girls_url_nodes = @school.xpath('//*[@id="school_menu_container"]/table/tr/td[2]/div/div/table/tr[3]/td/a')
+    # @girls_url_finally = ""
+    # @girls_url_nodes.each do |node|
+    #     if node.text.to_s == "Basketball"
+    #         @girls_url_finally = node["href"]
+    #     end
+    # end
 
 
 #go to the sub-page
 
-@roster_page = Nokogiri::HTML(open('#{@girls_url_finally}', 'User-Agent' => '#{browser}'))
+# @roster_page = Nokogiri::HTML(open('#{@girls_url_finally}', 'User-Agent' => '#{browser}'))
+
+#add team to database
 
 #add the roster to an array
 
-@roster_array = []
+# @roster_array = []
 
-@roster_html = @roster_page.css('#tbl_roster td')
-@roster_html.each do |data|
-    @roster_array << data.text
-end
+# @roster_html = @roster_page.css('#tbl_roster td')
+# @roster_html.each do |data|
+#     @roster_array << data.text
+# end
 
-#iterate and create objects in the database associated
+#add players to database with the above team id to track them back to the team
+
+
 
 #Home Away Height Position Year Games Pts Avg
+# i = 0    
+# player = []
+# @final_roster = []
+# while i < @roster_array.length
+#     counter = 0
 
+
+#     player << @roster_array[counter]
+#     i += 1
+#     counter += 1
+
+#     if counter == 7
+#         counter = 0
+#         @final_roster << player
+#         player = []
+#     end
+# end
 
 
 binding.pry
