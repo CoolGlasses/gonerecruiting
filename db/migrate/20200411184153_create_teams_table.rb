3ca1f51ac498e8e57333c0c85d52b604eafbc45a
Robin Andrea <robinandrea_2@yahoo.com>
2021-01-02 09:39:31 -0800

    changing migration because of an apparent setup issue

--- db/migrate/20200411184153_create_teams_table.rb
+++ db/migrate/20200411184153_create_teams_table.rb
@@ -4,7 +4,6 @@ class CreateTeamsTable < ActiveRecord::Migration[5.2]
       t.integer :school_id
       t.integer :coach_id
       t.integer :club_id
-      t.string :updated_at
 
       t.timestamps
     end
